ff9485cba780f4d3dc46d72d9593821dc7fa48fd
Abdulaziz <abdulazizmirabdullayev.20@gmail.com>
2023-04-03 22:31:24 +0500

    INFRA: Upload project

--- Program.cs
+++ Program.cs
@@ -1,15 +1,26 @@
 ﻿using KESCHA.Classes;
-Animal kescha = new Animal(name: "Kescha", age: 5);
-kescha.Security();
 
-Console.Write("\t HEY WELCOME \nPlease input your name: ");
-string userName = Console.ReadLine();
-Console.Write("Please input your age: ");
-short userAge = Convert.ToInt16(Console.ReadLine());
-kescha.CalculateAgeDifferenceNoReturn(userAge);
-// Console.WriteLine(kescha.Name);
-// Console.WriteLine(kescha.Age);
-kescha.PrintAgeDifference();
-kescha.CompareAges(userAge,userName);
-kescha.TellAboutFriends(userName,userAge);
-// System.Console.WriteLine(yourAge >= kescha.Age ?yourAge > kescha.Age ?"Siz Keschadan kattasiz":"Sizning kescha bilan yoshingiz teng" : "Siz Keschadan kichkinasiz ");
+namespace KESCHA
+{
+    class Program
+    {
+        static void Main()
+        {
+            Animal kescha = new Animal(name: "Kescha", age: 5);
+            kescha.Security();
+
+            Console.Write("\t HEY WELCOME \nPlease input your name: ");
+            string userName = Console.ReadLine();
+            Console.Write("Please input your age: ");
+            short userAge = Convert.ToInt16(Console.ReadLine());
+            kescha.CalculateAgeDifferenceNoReturn(userAge);
+            // Console.WriteLine(kescha.Name);
+            // Console.WriteLine(kescha.Age);
+            kescha.PrintAgeDifference();
+            kescha.CompareAges(userAge,userName);
+            kescha.TellAboutFriends(userName,userAge);
+            // System.Console.WriteLine(yourAge >= kescha.Age ?yourAge > kescha.Age ?"Siz Keschadan kattasiz":"Sizning kescha bilan yoshingiz teng" : "Siz Keschadan kichkinasiz ");
+
+        }
+    }
+}
